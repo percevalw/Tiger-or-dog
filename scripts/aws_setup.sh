babcf024546b3016aeafee35bf4e0253ade9b5cc
Perceval WAJSBURT <perceval.wajsburt@supelec.fr>
2017-02-09 23:20:15 +0100

    added a script for easy aws setup

---
+++ scripts/aws_setup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Forked from https://github.com/Avsecz/aws-tensorflow-setup
+
+# stop on error
+set -e
+############################################
+# install into /mnt/bin
+sudo mkdir -p /mnt/bin
+sudo chown ubuntu:ubuntu /mnt/bin
+
+# install the required packages
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get -y install linux-headers-$(uname -r) linux-image-extra-`uname -r`
+
+# install cuda
+sudo apt-get update
+sudo apt-get install -y cuda
+
+# get cudnn
+CUDNN_FILE=cudnn-8.0-linux-x64-v5.1-2.tgz
+wget https://dl.dropboxusercontent.com/s/dr7r06nyztquub4/${CUDNN_FILE}
+tar xvzf ${CUDNN_FILE}
+rm ${CUDNN_FILE}
+sudo cp cuda/include/cudnn.h /usr/local/cuda/include # move library files to /usr/local/cuda
+sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
+sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
+rm -rf cuda
+
+# set the appropriate library path
+echo 'export CUDA_HOME=/usr/local/cuda
+export CUDA_ROOT=/usr/local/cuda
+export PATH=$PATH:$CUDA_ROOT/bin:$HOME/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_ROOT/lib64
+' >> ~/.bashrc
+
+# install anaconda
+wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
+bash Anaconda3-4.0.0-Linux-x86_64.sh -b -p /mnt/bin/anaconda3
+rm Anaconda3-4.0.0-Linux-x86_64.sh
+echo 'export PATH="/mnt/bin/anaconda3/bin:$PATH"' >> ~/.bashrc
+echo 'export LANGUAGE=en_US.UTF-8' >> ~/.bashrc
+echo 'export LANG=en_US.UTF-8' >> ~/.bashrc
+echo 'export LC_ALL=en_US.UTF-8' >> ~/.bashrc
+
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+sudo dpkg-reconfigure locales
+
+# install tensorflow
+/mnt/bin/anaconda3/bin/pip install tensorflow-gpu
+/mnt/bin/anaconda3/bin/pip install git+git://github.com/tflearn/tflearn.git
+
+# install monitoring programs
+sudo wget https://git.io/gpustat.py -O /usr/local/bin/gpustat
+sudo chmod +x /usr/local/bin/gpustat
+sudo nvidia-smi daemon
+sudo apt-get -y install htop
+
+# reload .bashrc
+exec bash
+############################################
+# run the test
+# byobu				# start byobu + press Ctrl + F2
+# htop				# run in window 1, press Shift + F2
+# watch --color -n1.0 gpustat -cp	# run in window 2, press Shift + <left>
+# wget https://raw.githubusercontent.com/tensorflow/tensorflow/master/tensorflow/models/image/mnist/convolutional.py
+# python convolutional.py		# run in window 3
